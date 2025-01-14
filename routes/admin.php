858699a2ed0778afff7202a55d13ea0eebd32373
Md. Sabbir Hasan <sabbirhasan.bioxin@gmail.com>
2025-01-14 13:05:54 +0600

    dashboard-setup

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin',[AdminController::class,'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
